c1f7222abeb463d82949b7f50c677fc0ab0cb8da
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-12-02 12:42:11 +0100

    Chores: Added logger

--- src/main/java/com/student/SchoolApplication.java
+++ src/main/java/com/student/SchoolApplication.java
@@ -1,5 +1,7 @@
 package com.student;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchoolApplication {
 
+//	logger
+	private static final Logger logger = LoggerFactory.getLogger(SchoolApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("***********************************************************Starting the application");
+		logger.info("***********************************************************Starting the application");
+		logger.info("***********************************************************Starting the application");
 
 		SpringApplication.run(SchoolApplication.class, args);
 	}
